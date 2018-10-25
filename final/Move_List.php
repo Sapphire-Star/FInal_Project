64a1547fc4aa3190889fe072c0ae298719858ccc
Justin Kennedy <GrandJustinKennedy@yahoo.com>
2018-10-25 14:31:10 -0400

    final project

---
+++ final/Move_List.php
@@ -0,0 +1,51 @@
+<html>
+
+<head>
+	<title>
+		Moves List
+	</title>
+	
+	<link rel="stylesheet" href="assets/style.css">
+</head>
+
+<body>
+	<?php
+	require_once('assets/partials/nav.php');
+?>
+	<h1 style="color: orange;">Moves Lists</h1>
+	<hr>
+	<?php
+		echo "<button>+Arte</button>";
+	//what the f@!&!?!?
+	# this is also a single line comment (|-_-|)
+	/*Hwoh my god!... dis can multi line comment just like js.. mother&%$#!*/
+	?>
+	
+	
+	<form>
+	<input type="text" name="ArteName" placeholder="Type your Arte here!">
+	<input type="submit" value="submit">
+	</form>
+	<Table style="border-color: orange">
+		<tr style="border-color: red">
+			<th> column 1</th>
+			<th> column 2</th>
+			<th> column 3</th>
+		</tr>
+	</Table>
+
+	<?php
+	
+	
+?>
+
+
+
+
+
+
+
+
+</body>
+
+</html>
---
+++ final/assets/partials/nav.php
@@ -0,0 +1,17 @@
+<nav>
+	<h1>PHP Study</h1>
+	<ul>
+		<li>
+			<a href="index.php">HOME</a>
+		</li>
+		<li>
+			<a href="profile.php">PROFILES</a>
+		</li>
+		<li>
+			<a href="cheatsheet.php">CHEATSHEET</a>
+		</li>
+		<li>
+			<a href="Move_List.php">Moves List</a>
+		</li>
+	</ul>
+</nav>
